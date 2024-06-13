0af352826349dfbcb4106773871d4d4e046f3666
Raouf-blip <abdoul-raouf.temirov2@etu.univ-lorraine.fr>
2024-06-13 13:08:57 +0200

    fonctions 1 et 2

--- serveur/src/HttpServer.java
+++ serveur/src/HttpServer.java
@@ -1,25 +1,49 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
 import java.net.*;
 import java.nio.file.*;
 
 public class HttpServer {
+
+    private static int port = 80;
+    private static String rootDir = "site";
+
+    private static void lireConfiguration() {
+        try {
+            File configFile = new File("./etc/myweb/myweb.conf");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(configFile);
+            doc.getDocumentElement().normalize();
+
+            Element rootElement = (Element) doc.getElementsByTagName("webconf").item(0);
+
+            port = Integer.parseInt(rootElement.getElementsByTagName("port").item(0).getTextContent());
+            rootDir = rootElement.getElementsByTagName("root").item(0).getTextContent();
+
+            System.out.println("Configuration lue : port=" + port + ", rootDir=" + rootDir);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Erreur de lecture du fichier de configuration, utilisation des valeurs par défaut : port=" + port + ", rootDir=" + rootDir);
+        }
+    }
+
     private static void envoie_erreur(OutputStream out, int code, String message) throws IOException {
         String response = "HTTP/1.1 " + code + " " + message + "\r\n\r\n" + message;
         out.write(response.getBytes());
         out.flush();
     }
+
     public static void main(String[] args) {
-        int port;
-        if (args.length != 1) {
-            port = 80; 
-            System.out.println("Port par défaut 80");
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
+        lireConfiguration(); // Lire la configuration avant de démarrer le serveur
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("En attente d'une connexion sur le port " + port + "...");
-            System.out.println("http://127.0.0.1:"+port);
+            System.out.println("http://127.0.0.1:" + port);
 
             while (true) {
                 try (Socket clientSocket = serverSocket.accept()) {
@@ -27,8 +51,6 @@ public class HttpServer {
 
                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                     OutputStream out = clientSocket.getOutputStream();
-
-                    
 
                     String ligne = in.readLine();
 
@@ -43,16 +65,14 @@ public class HttpServer {
                     String version = lignecoupe[2];
 
                     if (page.equals("/")) {
-                        page = "/index.html"; 
+                        page = "/index.html";
                     }
 
-                    System.out.println("Requete de type " + methode + " reçu\nPour acceder à la page "+page+"\nEn version "+version);
-
-
+                    System.out.println("Requete de type " + methode + " reçu\nPour acceder à la page " + page + "\nEn version " + version);
 
                     page = page.substring(1);
 
-                    File file = new File("site/"+page);
+                    File file = new File(rootDir + "/" + page);
                     if (!file.exists()) {
                         envoie_erreur(out, 404, "Le fichier n'existe pas");
                         continue;
@@ -60,19 +80,14 @@ public class HttpServer {
 
                     byte[] contenue = Files.readAllBytes(file.toPath());
 
-
                     String typecontenue;
-                    if (page.contains(".html")){
+                    if (page.contains(".html")) {
                         typecontenue = "text/html";
-                    }
-                    else if (page.contains(".jpg")){
+                    } else if (page.contains(".jpg")) {
                         typecontenue = "image/jpg";
-                    }
-                    else if (page.contains(".gif"))
-                    {
+                    } else if (page.contains(".gif")) {
                         typecontenue = "image/gif";
-                    }
-                    else{
+                    } else {
                         envoie_erreur(out, 400, "Fichier d'un type inconnu");
                         continue;
                     }
